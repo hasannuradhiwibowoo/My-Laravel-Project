3a605820050f189a742ba8049f4d2f2a7bf6df7b
hasannuradhiwibowoo <hasannuradhiwibowo@gmail.com>
2026-09-20 22:10:47 +0700

    anjay

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,6 +9,6 @@ class ProductController extends Controller
     public function index()
     {
         $products = \App\Models\Product::all();
-        return $products;
+        return view('products.index');
     }
 }
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,2 @@
+<h1>Toko Sepatu</h1>
+<p>Daftar Produk</p>
