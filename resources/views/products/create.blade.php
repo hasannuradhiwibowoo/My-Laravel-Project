4955833fb37190ee689afedf871a091da15cce3e
hasannuradhiwibowoo <hasannuradhiwibowo@gmail.com>
2026-09-23 09:22:45 +0700

    anjay

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,9 +1,21 @@
 <h1>Tambah Produk</h1>
-<form>
-    <input type="text" placeholder="Nama Produk">
-    <input type="text" placeholder="Kategori Produk">
-    <input type="number" placeholder="Harga Produk">
-    <input type="number" placeholder="Stok Produk">
-
+<form action="/products" method="POST">
+    @csrf
+    <div>
+        <label>Nama Produk</label>
+        <input type="text" name="name">
+    </div>
+    <div>
+        <label>Kategori</label>
+        <input type="text" name="category">
+    </div>
+    <div>
+        <label>Harga</label>
+        <input type="text" name="price">
+    </div>
+    <div>
+        <label>Stok</label>
+        <input type="text" name="stock">
+    </div>
     <button type="submit">Simpan</button>
 </form>
